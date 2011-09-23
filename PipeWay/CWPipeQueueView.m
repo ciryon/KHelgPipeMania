4e5dc36549352d5117318ebdbcbfbf6b8142a8c6
Ester Ytterbrink <ester.ytterbrink@jayway.com>
2011-09-23 16:52:16 +0200

    Fix one crash

--- PipeWay/CWPipeQueueView.m
+++ PipeWay/CWPipeQueueView.m
@@ -24,7 +24,7 @@
 }
 -(void)layoutSubviews;
 {
-
+    
     NSUInteger numberOfPipes = [self.datasource numberOfItemsInQueue:self];
     float width = self.frame.size.width;
     float height = self.frame.size.height;
