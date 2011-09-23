ec151f1b8ebd43ca89fc13c3ea042f99d0ffa201
Ester Ytterbrink <ester.ytterbrink@jayway.com>
2011-09-23 14:45:44 +0200

    Added types and init.

--- PipeWay/CWPipe.h
+++ PipeWay/CWPipe.h
@@ -11,11 +11,16 @@
 typedef enum {
   CWPipeTypeVertical = 0,     // |
   CWPipeTypeHorizontal = 1,   // -
-  CWPipeTypeWestNorth = 2,    // _|
-  CWPipeTypeNorthEast = 3     // |_
+  CWPipeTypeNorthWest = 2,    // _|
+  CWPipeTypeNorthEast = 3,    // |_
+    CWPipeTypeSouthWest = 4,
+    CWPipeTypeSouthEast = 5, 
+    CWPipeTypeCross = 6
   } CWPipeType;
 
 
 @interface CWPipe : NSObject
+-(id)initWithType:(CWPipeType) type;
+@property (nonatomic, assign) CWPipeType type;
 
 @end
